93e743954abf328ee3103bb5a085b24bc6ee0cf2
kim1387 <ce19f003@naver.com>
2021-02-05 00:41:51 +0900

    rest 개념
    http 프로토콜에 있는 Method를 활용한 아키텍처 스타일이다.
    HTTP Method를 통해서 Resource를 처리한다.
    CRUD를 통한 Resource 조작할때만 사용한다.
    
    get 조회
    /user/{id}
    post 생성
    body로 감싸서 데이터가 주소에 보이지 않음
    
    put 수정
    post와 마찬가지로 데이터가 Body에 들어가 있으며 주로 업데이트에 사용
    
    patch 수정
    패치할때 사용
    
    delte 삭제
    get과 마찬가지로 주소에 파라미터가 들어가며 삭제할때 사용
    LOMBOK + JPA

---
+++ src/main/java/com/example/study/model/entinty/User.java
@@ -0,0 +1,37 @@
+package com.example.study.model.entinty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// @Table(name = "user") 테이블명과 클래스명이 같다면 선언 안해도 됌
+@Data // lombok 자동으로 constructor와 dependable method 선언
+@AllArgsConstructor // 모든 매개변수가 들어가는 constructor
+@NoArgsConstructor // 디폴트 생성자?
+@Entity
+public class User {
+
+    @Id//식별자
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//관리 전략 옵션
+    private Long id;
+
+    //@Column(name = "account") java 컬럼의 변수 이름과 db 컬럼 변수의 이름이 다르다면 써준다.
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+    // 1 : N
+}
